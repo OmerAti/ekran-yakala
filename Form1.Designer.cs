668a3465bdc524fca5de9d3fc77b9f09aab373e5
Ömer ATABER <67821076+OmerAti@users.noreply.github.com>
2024-07-15 23:17:46 +0300

    Create Form1.Designer.cs

---
+++ Form1.Designer.cs
@@ -0,0 +1,18 @@
+namespace Ekran_Al
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+      
+        #region Windows Form Designer generated code
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "Form1";
+        }
+
+        #endregion
+    }
+}
